f1b4860bffcf5d155fe2e9f15bc4bc83ed0fbfb1
Asilbek <genesisdestr@gmail.com>
2024-11-11 11:44:56 +0500

    for 2 added

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@ public class Main {
         for (int i = 0; i <=10; i++) {
             System.out.println(i);
         }
+        System.out.println("2 in progress");
+
+        for (int i = 0; i <=20; i++) {
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
     }
+
 }
