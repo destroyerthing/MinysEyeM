66667ebeb7718859b917a8f3107fce8c2ff9ee64
Asilbek <genesisdestr@gmail.com>
2024-11-11 11:50:05 +0500

    for 3 added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i <=10; i++) {
-            System.out.println(i);
+//        for (int i = 0; i <=10; i++) {
+//            System.out.println(i);
+//        }
+//        System.out.println("2 in progress");
+//
+//        for (int i = 0; i <=20; i++) {
+//            if (i%2==0){
+//                System.out.println(i);
+//            }
+//        }
+       int sum = 0;
+        for (int i = 1; i < 100; i++) {
+            sum+=i;
         }
-        System.out.println("2 in progress");
-
-        for (int i = 0; i <=20; i++) {
-            if (i%2==0){
-                System.out.println(i);
-            }
-        }
+        System.out.println(sum);
+        
     }
 
 }
